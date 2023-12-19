a6e18fad69fa260a645d65da15d0f589862e530a
zihang0403 <zihang0403@qq.com>
2023-12-19 11:32:30 +0800

    v0

--- sql/edumanagement.sql
+++ sql/edumanagement.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : zihang
+ Source Server         : Zihang
  Source Server Type    : MySQL
  Source Server Version : 80023
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 80023
  File Encoding         : 65001
 
- Date: 17/12/2023 22:41:54
+ Date: 19/12/2023 11:32:13
 */
 
 SET NAMES utf8mb4;
@@ -25,14 +25,17 @@ CREATE TABLE `courseinfo`  (
   `courseid` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `coursescore` float NULL DEFAULT NULL,
-  `courseperiod` int(0) NULL DEFAULT NULL,
+  `courseperiod` int NULL DEFAULT NULL,
+  `institute` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`courseid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of courseinfo
 -- ----------------------------
-INSERT INTO `courseinfo` VALUES ('10001', '高等数学', 4.5, 64);
+INSERT INTO `courseinfo` VALUES ('1', '1', 1, 1, '计算机与信息工程学院');
+INSERT INTO `courseinfo` VALUES ('10001', '高等数学', 4.5, 64, '计算机与信息工程学院');
+INSERT INTO `courseinfo` VALUES ('10002', '数据结构', 4.5, 64, '计算机与信息工程学院');
 
 -- ----------------------------
 -- Table structure for courseset
@@ -43,16 +46,16 @@ CREATE TABLE `courseset`  (
   `studentid` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `teacherid` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `courseweekday` set('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
-  `starttime` time(0) NULL DEFAULT NULL,
-  `endtime` time(0) NULL DEFAULT NULL,
+  `starttime` time NULL DEFAULT NULL,
+  `endtime` time NULL DEFAULT NULL,
   `classroom` char(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   INDEX `studentid`(`studentid`) USING BTREE,
   INDEX `courseid`(`courseid`) USING BTREE,
   INDEX `teacherid`(`teacherid`) USING BTREE,
-  CONSTRAINT `studentid` FOREIGN KEY (`studentid`) REFERENCES `studentinfo` (`studentid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `courseid` FOREIGN KEY (`courseid`) REFERENCES `courseinfo` (`courseid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `studentid` FOREIGN KEY (`studentid`) REFERENCES `studentinfo` (`studentid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
   CONSTRAINT `teacherid` FOREIGN KEY (`teacherid`) REFERENCES `teacherinfo` (`teacherid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of courseset
@@ -71,7 +74,7 @@ CREATE TABLE `studentinfo`  (
   `bornday` date NULL DEFAULT NULL,
   `institute` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`studentid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of studentinfo
@@ -93,13 +96,12 @@ CREATE TABLE `teacherinfo`  (
   `position` char(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `institute` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `bornday` date NULL DEFAULT NULL,
-  `teachcourse` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`teacherid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of teacherinfo
 -- ----------------------------
-INSERT INTO `teacherinfo` VALUES ('10001', '123456', ' 王强', '男', '副教授', '计算机与信息工程学院', '2023-02-21', NULL);
+INSERT INTO `teacherinfo` VALUES ('10001', '123456', ' 王强', '男', '副教授', '计算机与信息工程学院', '2023-02-21');
 
 SET FOREIGN_KEY_CHECKS = 1;
